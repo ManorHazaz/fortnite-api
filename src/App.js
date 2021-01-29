49cd07ab27f2c13092afc6d9bdeb4a394815f065
manor hagage <manorhagage@gmail.com>
2021-01-29 18:36:07 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import { React , useState, useEffect } from 'react';
+
 import './App.css';
+import Weapon from './components/Weapon';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [weapons,setWeapons] = useState([]);
+
+	useEffect(() => {
+		const headers = { 'Authorization': 'bca82a6a-7397e9e5-6beeac7d-2ad5882b' }
+		fetch('https://fortniteapi.io/v1/loot/list?lang=en', { headers })
+			.then(response => response.json())
+			.then(data => setWeapons( data.weapons));
+	}, []);
+
+	return (
+		<div className="App">
+			<div className='weapons'>
+				{
+					weapons.slice(0,20).map( weapon => 
+					(
+						<Weapon key={weapon.id} weapon={weapon} />
+					))
+				}
+			</div>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Weapon.js
@@ -0,0 +1,67 @@
+import LightBullets from '../static/Light-bullets.png'; 
+import MediumBullets from '../static/Medium-bullets.png'; 
+import HeavyBullets from '../static/Heavy-bullets.png'; 
+
+function Weapon({weapon}) {
+
+    function bulletType(weapon) 
+    {
+        const searchTag = weapon.searchTags;
+        
+        if(searchTag.includes("LightBullets"))
+        {
+            return 'Light bullets';
+        }
+
+        if(searchTag.includes("MediumBullets"))
+        {
+            return 'Medium bullets';
+        }
+
+        if(searchTag.includes("HeavyBullets"))
+        {
+            return 'Heavy bullets';
+        }
+
+        return 'Unknown';
+    }
+
+    function bulletTypeImg(bulletType)
+    {
+        switch (bulletType) 
+        {
+            case 'Light bullets':
+                return LightBullets;
+            break;
+
+            case 'Medium bullets':
+                return MediumBullets;
+            break;
+
+            case 'Heavy bullets':
+                return HeavyBullets;
+            break;
+        
+            default:
+                return LightBullets;
+            break;
+        }
+    }
+
+    return (
+        <div className={`weapon ${weapon.rarity}`}>
+            <div className='weapon-data'>
+                <div className='name'>{ weapon.name }</div>
+                <div className='demege'>{ weapon.mainStats.DmgPB }</div>
+                <div className='bullet-type'>{ bulletType(weapon) }</div>
+            </div>
+            {console.log(weapon)}
+            
+            
+            <img src={ weapon.images.icon }/>
+            
+        </div>
+    );
+}
+
+export default Weapon
